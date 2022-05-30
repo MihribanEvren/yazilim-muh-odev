30bf0c5fb2126606b068bb1511dad17671127328
Mihriban Evren <mihribanevrenn@gmail.com>
2022-05-30 21:38:07 +0300

    hesapmakinesi

--- BasitHesapMakine.java
+++ BasitHesapMakine.java
@@ -1,3 +1,4 @@
+
 package basithesapmakine;
 
 import java.util.Scanner;
@@ -10,7 +11,9 @@ public class BasitHesapMakine {
         
         System.out.println("**********************************");
         String islemler = "1.Toplama işlemi\n" 
-                         +"2.Çıkarma işlemi\n";
+                         +"2.Çıkarma işlemi\n" 
+                         +"3.Çarpma işlemi\n" 
+                         +"4.Bölme işlemi\n";
         System.out.println(islemler);
         System.out.println("**********************************");
         System.out.println("Yapmak istediğiniz işlemi seçiniz:");
@@ -32,6 +35,22 @@ public class BasitHesapMakine {
                 sayi2 = scanner.nextInt();
                 int çıkarma = sayi1-sayi2;
                 System.out.println("Fark= " + çıkarma);
+                break;
+            case "3": 
+                System.out.println("1. sayı:");
+                sayi1 = scanner.nextInt();
+                System.out.println("2.sayı:");
+                sayi2 = scanner.nextInt();
+                int çarpım = sayi1*sayi2;
+                System.out.println("Çarpımları= " + çarpım);
+                break;
+            case "4": 
+                System.out.println("1. sayı:");
+                double sayi3 = scanner.nextInt();
+                System.out.println("2.sayı:");
+                double sayi4 = scanner.nextInt();
+                double bölüm = sayi3/sayi4;
+                System.out.println("Bölümleri:= " + bölüm);
                 break;
             default:
                 System.out.println("Geçersiz işlem...");
