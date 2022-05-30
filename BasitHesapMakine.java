a754da06e7dea30b730d0a2c667b24400c4ea438
Mihriban Evren <mihribanevrenn@gmail.com>
2022-05-30 22:05:24 +0300

    solid ve clean kod ile düzenlendi

--- BasitHesapMakine.java
+++ BasitHesapMakine.java
@@ -7,59 +7,65 @@ public class BasitHesapMakine {
 
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        
-        System.out.println("**********************************");
-        String islemler = "1.Toplama işlemi\n" 
-                         +"2.Çıkarma işlemi\n" 
-                         +"3.Çarpma işlemi\n" 
-                         +"4.Bölme işlemi\n";
-        System.out.println(islemler);
-        System.out.println("**********************************");
-        System.out.println("Yapmak istediğiniz işlemi seçiniz:");
-        String islem = scanner.nextLine();
-        
-        switch(islem){
-            case "1": 
-                System.out.println("1. sayı:");
-                int sayi1 = scanner.nextInt();
-                System.out.println("2.sayı:");
-                int sayi2 = scanner.nextInt();
-                int toplam = sayi1+sayi2;
-                System.out.println("Toplamları= " + toplam);
+
+        {
+            double sayi1, sayi2;
+      
+            Scanner scanner = new Scanner(System.in);
+      
+            System.out.println("Sayıları gir:");
+      
+            sayi1 = scanner.nextDouble();
+      
+            sayi2 = scanner.nextDouble();
+      
+            System.out.println("İşlemi seç: (+,-,*,/)");
+      
+            char islem = scanner.next().charAt(0);
+      
+            double sonuc = 0;
+      
+            switch (islem) {
+      
+            case '+':
+      
+                sonuc = sayi1 + sayi2;
+      
                 break;
-            case "2": 
-                System.out.println("1. sayı:");
-                sayi1 = scanner.nextInt();
-                System.out.println("2.sayı:");
-                sayi2 = scanner.nextInt();
-                int çıkarma = sayi1-sayi2;
-                System.out.println("Fark= " + çıkarma);
+
+            case '-':
+      
+                sonuc = sayi1 - sayi2;
+      
                 break;
-            case "3": 
-                System.out.println("1. sayı:");
-                sayi1 = scanner.nextInt();
-                System.out.println("2.sayı:");
-                sayi2 = scanner.nextInt();
-                int çarpım = sayi1*sayi2;
-                System.out.println("Çarpımları= " + çarpım);
+      
+            case '*':
+      
+                sonuc = sayi1 * sayi2;
+      
                 break;
-            case "4": 
-                System.out.println("1. sayı:");
-                double sayi3 = scanner.nextInt();
-                System.out.println("2.sayı:");
-                double sayi4 = scanner.nextInt();
-                double bölüm = sayi3/sayi4;
-                System.out.println("Bölümleri:= " + bölüm);
+      
+            case '/':
+      
+                sonuc = sayi1 / sayi2;
+      
                 break;
+      
             default:
-                System.out.println("Geçersiz işlem...");
-        
-        
-        
+      
+                System.out.println("Hatalı işlem.");
+      
+                break;
+            }
+      
+            System.out.println("Sonuç:");
+      
+            System.out.println();
+      
+            System.out.println(sayi1 + " " + islem + " " + sayi2
+                               + " = " + sonuc);
         }
         
-    
-    
-}
+    }
+           
 }
